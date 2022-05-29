19ddff21c98e2be6c874090085d63a6d2462f950
R Yokoyama <yoko@arunoMacBook-Pro.local>
2022-05-29 13:40:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const NameText = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>Hi, {props.name}!</p>
+  )
 }
+
+const App = () => {
+  return (
+    <div>
+      <NameText name="Jack" />
+      <NameText name="Mary" />
+    </div>
+  )
+}
+
 
 export default App;
